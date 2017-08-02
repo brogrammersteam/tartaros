6f3a57299484421257ecfe9714216a1669d3afeb
schmalz2000 <sebi.2000@t-online.de>
2017-08-02 20:37:12 +0200

    Finished the Pre-Release Warning Screen.

--- core/src/com/brogrammers/tartaros/screens/SplashScreen.java
+++ core/src/com/brogrammers/tartaros/screens/SplashScreen.java
@@ -3,12 +3,13 @@ package com.brogrammers.tartaros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.brogrammers.tartaros.Tartaros;
 
@@ -21,32 +22,50 @@ public class SplashScreen implements Screen {
 
     private Stage stage;
 
-    private Image splashImage;
+    private Label warningLabel;
+    private Label tapLabel;
+
+    private Label.LabelStyle warningLabelStyle;
+    private Label.LabelStyle tapLabelStyle;
+
+    private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter warningLabelParameter;
+    private FreeTypeFontGenerator.FreeTypeFontParameter tapLabelParameter;
 
     public SplashScreen(Tartaros game) {
         this.game = game;
         this.stage = new Stage(new FitViewport(Tartaros.V_WIDTH, Tartaros.V_HEIGHT, game.camera));
         Gdx.input.setInputProcessor(stage);
 
-        Texture splashTexture = new Texture(Gdx.files.internal("badlogic.jpg"));
-        splashImage = new Image(splashTexture);
-        splashImage.setOrigin(splashImage.getWidth()/2, splashImage.getHeight()/2);
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Roboto-Bold.ttf"));
 
-        stage.addActor(splashImage);
+        warningLabelParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        warningLabelParameter.size = 50;
+
+        tapLabelParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        tapLabelParameter.size = 30;
+
+        BitmapFont warningLabelFont = generator.generateFont(warningLabelParameter);
+        BitmapFont tapLabelFont = generator.generateFont(tapLabelParameter);
+
+        warningLabelStyle = new Label.LabelStyle(warningLabelFont, new Color(1, 1, 1, 1));
+        tapLabelStyle = new Label.LabelStyle(tapLabelFont, new Color(1, 1, 1, 1));
+
+        warningLabel = new Label("Please consider that this is a Pre-Release Version of the Game.", warningLabelStyle);
+        tapLabel = new Label("Tap to get to the main screen", tapLabelStyle);
+
+        stage.addActor(warningLabel);
+        stage.addActor(tapLabel);
     }
 
     @Override
     public void show() {
         System.out.println("Show");
-        splashImage.setPosition(400,300);
+        warningLabel.setPosition((Tartaros.V_WIDTH - warningLabel.getWidth()) / 2, (Tartaros.V_HEIGHT - warningLabel.getHeight()) / 2 + 200);
+        tapLabel.setPosition((Tartaros.V_WIDTH - tapLabel.getWidth()) / 2, (Tartaros.V_HEIGHT - tapLabel.getHeight()) / 2);
 
-//        splashImage.addAction(sequence(alpha(0f),
-//                parallel(moveBy(30, -20, 2f), fadeIn(5f))));
-
-        splashImage.addAction(sequence(alpha(0f), scaleTo(.1f, .1f),
-                parallel(fadeIn(2f, Interpolation.pow2),
-                        scaleTo(2f,2f,2.5f, Interpolation.pow5),
-                        moveTo(stage.getWidth() / 2 -32, stage.getHeight() / 2 - 32, 2f, Interpolation.swing))));
+        warningLabel.addAction(sequence(alpha(0f), fadeIn(1.5f)));
+        tapLabel.addAction(sequence(alpha(0f), forever(sequence(fadeIn(1.5f), fadeOut(1.5f)))));
     }
 
     @Override
@@ -57,10 +76,6 @@ public class SplashScreen implements Screen {
         update(delta);
 
         stage.draw();
-
-        game.batch.begin();
-        game.font.draw(game.batch, "Splashscreen!", 120,120);
-        game.batch.end();
     }
 
     public void update(float delta){
@@ -71,6 +86,9 @@ public class SplashScreen implements Screen {
     public void handleInput(){
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
             app.exit();
+        }
+        if(Gdx.input.justTouched()){
+            game.setScreen(new MenuScreen(game));
         }
     }
 
@@ -98,5 +116,6 @@ public class SplashScreen implements Screen {
     public void dispose() {
         System.out.println("Dispose");
         stage.dispose();
+        generator.dispose();
     }
 }
