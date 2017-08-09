a00e2da0b6eaaeb23c349eed72c34bed9494a4a0
schmalz2000 <sebi.2000@t-online.de>
2017-08-09 11:39:25 +0200

    Finished the Reset Dialog

--- core/src/com/brogrammers/tartaros/screens/SettingsScreen.java
+++ core/src/com/brogrammers/tartaros/screens/SettingsScreen.java
@@ -3,7 +3,10 @@ package com.brogrammers.tartaros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -35,6 +38,21 @@ public class SettingsScreen implements Screen {
 
     private TextButton resetButton;
     private TextButton impressumButton;
+    private TextButton resetAcceptButton;
+    private TextButton resetRejectButton;
+
+    private Label titleLabel;
+    private Label resetLabel;
+
+    private Label.LabelStyle titleLabelStyle;
+    private Label.LabelStyle resetLabelStyle;
+
+    private FreeTypeFontGenerator generator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter titleLabelParameter;
+    private FreeTypeFontGenerator.FreeTypeFontParameter resetLabelParameter;
+
+    private BitmapFont titleLabelFont;
+    private BitmapFont resetLabelFont;
 
     public SettingsScreen(Tartaros game) {
         this.game = game;
@@ -49,6 +67,23 @@ public class SettingsScreen implements Screen {
 
         resetDialog = new Dialog("Reset Game", skin);
 
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Fiolex_Mephisto.ttf"));
+
+        titleLabelParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        titleLabelParameter.size = 125;
+
+        resetLabelParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        resetLabelParameter.size = 25;
+
+        titleLabelFont = generator.generateFont(titleLabelParameter);
+        resetLabelFont = generator.generateFont(resetLabelParameter);
+
+        titleLabelStyle = new Label.LabelStyle(titleLabelFont, new Color(1,1,1,1));
+        titleLabel = new Label("Einstellungen", titleLabelStyle);
+
+        resetLabelStyle = new Label.LabelStyle(resetLabelFont, new Color(1,1,1,1));
+        resetLabel = new Label("Bist du dir sicher das du das Spiel zurücksetzten willst ?", resetLabelStyle);
+
         backgroundTexture = game.assets.get("graphics/background_menu_new.png", Texture.class);
         backgroundImage = new Image(backgroundTexture);
 
@@ -59,6 +94,8 @@ public class SettingsScreen implements Screen {
 
         resetButton = new TextButton("Reset Game", skin);
         impressumButton = new TextButton("Created by", skin);
+        resetAcceptButton = new TextButton("Ok", skin);
+        resetRejectButton = new TextButton("Abbrechen", skin);
 
 
         stage.addActor(backgroundImage);
@@ -83,6 +120,12 @@ public class SettingsScreen implements Screen {
             }
         });
 
+        resetDialog.text(resetLabel);
+        resetDialog.setResizable(false);
+        resetDialog.setMovable(false);
+        resetDialog.button(resetAcceptButton);
+        resetDialog.button(resetRejectButton);
+
         resetButton.addListener(new ClickListener() {
             @Override
             public void touchUp(InputEvent e, float x, float y, int point, int button){
@@ -90,13 +133,26 @@ public class SettingsScreen implements Screen {
             }
         });
 
+        resetAcceptButton.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent e, float x, float y, int point, int button){
+                game.setScreen(new MenuScreen(game));
+            }
+        });
+
+        titleLabel.setAlignment(Align.center);
+
         table.addAction(sequence(alpha(Tartaros.alphaStart), fadeIn(Tartaros.fadeTime)));
 
-        table.add(languageSelectBox).expandX().padTop(20).height(100).width(300);
+        table.bottom();
         table.row();
-        table.add(resetButton).expandX().padTop(20).height(100).width(300);
+        table.add(titleLabel).expandX().padBottom(400).width(Tartaros.V_WIDTH - 100).center();
         table.row();
-        table.add(impressumButton).expandX().padTop(20).height(100).width(300);
+        table.add(languageSelectBox).expandX().padBottom(50).height(100).width(300);
+        table.row();
+        table.add(resetButton).expandX().padBottom(50).height(100).width(300);
+        table.row();
+        table.add(impressumButton).expandX().padBottom(75).height(100).width(300);
     }
 
     @Override
